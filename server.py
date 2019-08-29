25185f7c7ddf8fef09706a31a78897bd9d6619ec
hwchiu <hungwei@opennetworking.org>
2019-08-29 01:43:18 -0700

    init commit

---
+++ server.py
@@ -0,0 +1,36 @@
+import socket
+
+import socket
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+localIP     = "127.0.0.1"
+localPort   = 20001
+bufferSize  = 1024
+msgFromServer       = "server is " + get_ip()
+bytesToSend         = str.encode(msgFromServer)
+# Create a datagram socket
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# Bind to address and ip
+UDPServerSocket.bind((localIP, localPort))
+print("UDP server up and listening")
+# Listen for incoming datagrams
+while(True):
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    message = bytesAddressPair[0]
+    address = bytesAddressPair[1]
+    clientMsg = "Message from Client:{}".format(message)
+    clientIP  = "Client IP Address:{}".format(address)
+    print(clientMsg)
+    print(clientIP)
+    # Sending a reply to client
+    UDPServerSocket.sendto(bytesToSend, address)
